47534f3698f466348d90e40978b63475d90d08a2
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-06-11 22:17:09 -0400

    repair test passing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,52 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  // get,
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const success = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement + 1,
+    }
+    return success
+  } else {
+    const err = "Enhancement is above 20, no handouts"
+    return (err)
+  }
+ 
 }
 
 function fail(item) {
-  return { ...item };
+  const weakenD5 = item.durability - 5
+  const weakenD10 = item.durability - 10
+  const weakenE = item.enhancement - 1
+
+  if (item.enhancement < 15) {
+    return ({ ...item, durability: weakenD5 })
+  } else if (item.enhancement >= 15) {
+    return ({ ...item, durability: weakenD10 })
+  } else if (item.enhancement > 16) {
+    return ({ ...item, enhancement: weakenE })
+  } else {
+    return { ...item, };
+  }
 }
+
 
 function repair(item) {
-  return { ...item };
+  if (item.durability < 100) {
+    const mend = {
+      name: item.name,
+      durability: 100,
+      enhancement: item.enhancement, 
+    }
+    return mend
 }
 
-function get(item) {
-  return { ...item };
+// function get(item) {
+//   return { ...item };
+// }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,46 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require('./enhancer.js');
+
+const warHammer = {
+    name: "War Hammer",
+    durability: 75,
+    enhancement: 15
+}
+
+const leatherShield = {
+    name: "Leather Shield",
+    durability: 35,
+    enhancement: 10
+}
+
+const longSword = {
+    name: "Long Sword",
+    durability: 45,
+    enhancement: 20
+}
+
+const mace = {
+    name: "Mace",
+    durability: 85,
+    enhancement: 5
+}
+
+
+
 // test away!
+
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100
+
+describe('enchance repair items', () => {
+    describe('test repair', () => {
+        it('repair item to 100 durability', () => {
+            expect(repair(mace)).toEqual({ ...mace, durability: 100 })
+        })
+    })
+
+
+
+});
+
+
